f710f798a24d47bba479818bb2185998d8dd5109
Luís Alves <24353152+alves-luis@users.noreply.github.com>
2018-11-12 21:15:07 +0000

    Adicionada a pasta do projeto NetBeans

---
+++ projeto/ConfiguraFacil/src/configurafacil/ConfiguraFacil.java
@@ -0,0 +1,16 @@
+package configurafacil;
+
+/**
+ *
+ * Main class
+ */
+public class ConfiguraFacil {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
